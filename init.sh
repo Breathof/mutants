ba95002a0dd4993116fbef9d04885dfa1246d134
Clovis <nachofcz@mail.com>
2021-04-03 13:56:07 -0300

    Requirements

--- init.sh
+++ init.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 DATABASE=./adn.db
-DEPENDENCIES="flask requests waitress pytest-cov"
 
 # Create database
 if [[ ! -e "$DATABASE" ]]; then
     python3 data_base_init.py || python data_base_init.py
 fi
 
-pip3 install $DEPENDENCIES || pip install $DEPENDENCIES
+pip3 -r requirements.txt || pip -r requirements.txt
 
 python3 ./src/main.py || python ./src/main.py
